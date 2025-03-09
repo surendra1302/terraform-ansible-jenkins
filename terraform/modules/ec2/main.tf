6a6296e363f3bccc45f1410a5238a486c09fad67
surendra1302 <surendrakorivi96@gmail.com>
2025-03-10 01:07:56 +0530

    Update main.tf

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -4,6 +4,12 @@ resource "aws_instance" "ec2_instance" {
   subnet_id              = var.subnet_id
   key_name               = var.key_name
   associate_public_ip_address = true
+  user_data = <<-EOF
+    #!/bin/bash
+    apt update -y
+    apt install -y apache2
+    echo "Instance Configured" > /var/www/html/index.html
+  EOF
 
   tags = {
     Name = var.instance_name
